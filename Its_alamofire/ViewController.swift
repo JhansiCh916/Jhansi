24c162ccc95491971f05080be603ab09322db1b0
JhansiCh916 <jhansidevi.chundru@narayanagroup.com>
2022-08-17 15:42:14 +0530

    Initial Commit

---
+++ Its_alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Its_alamofire
+//
+//  Created by Jhansi Ch on 17/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
